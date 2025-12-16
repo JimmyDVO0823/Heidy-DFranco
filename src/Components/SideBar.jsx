2541ae940d286505f237b2fb28252789014e1209
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-12-16 18:00:05 -0500

    Hice la base del logo y el nav, ahora a terminar unos estilos y tengo listo ese primer componente

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,9 +1,12 @@
 function SideBar({ elements }) {
   return (
-    <nav className="sidebar">
+    <nav className="bg-gray-600 justify-between flex items-center">
       <ul>
+        <li className="text-white p-4 font-bold text-lg">Logo</li>
+      </ul>
+      <ul className="flex flex-row p-4 space-x-2 justify-end mr-10">
         {elements.map((element, index) => (
-          <li key={index}>
+          <li key={index} className="text-white hover:underline">
             <a href={element.link}>{element.title}</a>
           </li>
         ))}
